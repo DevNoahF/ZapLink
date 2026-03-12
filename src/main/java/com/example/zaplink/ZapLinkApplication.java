6b39ab6acc32c3bd5db6d28436826c7482c573c5
noah <noahvf16@outlook.com>
2026-03-12 13:10:33 -0300

    first commit

---
+++ src/main/java/com/example/zaplink/ZapLinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.zaplink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zaplink/ZapLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zaplink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
